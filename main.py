0a3265374a644da3c7b84d4f9d90d4b4fe6116da
Oleg Tverskoy <otverskoj@gmail.com>
2023-12-09 12:48:30 +0600

    users

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from fastapi import FastAPI
 app = FastAPI(title='Leaning FastAPI App')
 
 
-@app.get('/hello')
-def hello_handler():
-    return {'result': 'Hello World!'}
+USERS = [
+    {'id': 1, 'name': 'John', 'role': 'admin'},
+    {'id': 2, 'name': 'Sarah', 'role': 'teacher'},
+    {'id': 3, 'name': 'Antony', 'role': 'pupil'},
+]
+
+
+@app.get('/users/{users_id}')
+def get_user_by_id(user_id: int) -> dict[str, str | int] | None:
+    return next((item for item in USERS if item['id'] == user_id), None)
